d0cd4d37142059a54fa08d9d8f2141680efaf467
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-28 19:43:28 +0700

    fix navbar footer

--- src/pages/Company/registerPages.jsx
+++ src/pages/Company/registerPages.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import Forms from "../../components/Forms/Forms";
+
 const RegisterPages = () => {
-  return <div>registerPages</div>;
+  return (
+    <div className="py-52 flex flex-col min-h-screen justify-center items-center bg-[#E5E5E5]">
+      <p className="mb-5">
+        Apakah anda mencari pekerjaan?
+        <Link className="text-blue-500"> Klik di sini.</Link>
+      </p>
+      <form className="bg-white rounded-xl shadow-md">
+        <div>
+          <img
+            src="images/img-register.png"
+            alt=""
+            className="w-full rounded-t-xl"
+          />
+        </div>
+        <Forms />
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPages;
